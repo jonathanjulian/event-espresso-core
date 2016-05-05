8bd595669cbd1842ca809d8ef2e2fb97b690a9cb
Brent Christensen <brent@eventespresso.com>
2016-05-04 21:39:56 -0700

    don't duplicate jquery objects; [touch:9717]

--- core/templates/global_assets/scripts/EE_Error.js
+++ core/templates/global_assets/scripts/EE_Error.js
@@ -1,27 +1,29 @@
 jQuery(document).ready(function($) {
 
-	var message = $('#ee-error-message');
+	var $message = $('#ee-error-message');
+	var $notices = $('#espresso-notices');
+	var $body = $('body');
 	var target;
 
-	if ( message.length ) {
-		$('#ee-error-message').remove();
-		if ( $('#espresso-notices').size() ) {
-			target = $('#espresso-notices');
+	if ( $message.length ) {
+		$message.remove();
+		if ( $notices.size() ) {
+			target = $notices;
 		} else if ( $('#screen-meta-links').size() ) {
 			target = $('#screen-meta-links');
 		} else if ( $('#wpbody-content').size() ) {
 			target = $('#wpbody-content');
 		} else {
-			target = $('body');
+			target = $body;
 		}
 
-		$( target ).html( message ).css({ 'position' : 'relative', 'top' : 0, 'left' : 0 , 'margin' : '3em 1em 0 0' , 'z-index' : 5 });
+		$( target ).html( $message ).css({ 'position' : 'relative', 'top' : 0, 'left' : 0 , 'margin' : '3em 1em 0 0' , 'z-index' : 5 });
 		//set from wp_localize_script in php
 		if( ee_settings.wp_debug !== '1' ) {
 			$( '.ee-error-trace-dv').hide();
 		}
 
-		$('body').on( 'click', '.display-ee-error-trace-lnk', function(e) {
+		$body.on( 'click', '.display-ee-error-trace-lnk', function(e) {
 			e.preventDefault();
 			e.stopPropagation();
 			var traceTable = '#' + $(this).attr('rel') +'-dv';
