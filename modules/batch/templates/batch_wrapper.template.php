84a439b85bdd0aa14de168c577639696dac8f5f9
Josh Feck <josh@nxnweb.com>
2016-08-11 10:54:24 -0400

    change batch_file_runner.template to a php file [touch: 9971]

--- modules/batch/templates/batch_wrapper.template.php
+++ modules/batch/templates/batch_wrapper.template.php
@@ -4,5 +4,5 @@ $batch_request_type = EED_Batch::instance()->batch_request_type();
 if( $batch_request_type == EED_Batch::batch_job ) {
 	require( 'batch_runner.template.html' );
 } elseif( $batch_request_type == EED_Batch::batch_file_job ) {
-    require( 'batch_file_runner.template.html' );
+    require( 'batch_file_runner.template.php' );
 }
